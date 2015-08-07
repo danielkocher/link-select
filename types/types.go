d2483b58130416e1def02268399afb926eeab919
Daniel Kocher <Daniel.Kocher@stud.sbg.ac.at>
2015-08-07 17:12:18 +0200

    Fix Config type

--- types/types.go
+++ types/types.go
@@ -2,7 +2,7 @@ package types
 
 //type Config map[string][]map[string]string
 type ConfigRecord map[string]string
-type Config map[string][]ConfigRecord
+type Config map[string]ConfigRecord
 
 type ReadRecord struct {
 	Title string
