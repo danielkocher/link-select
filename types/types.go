3e7130184e7d0762b1152eb01b780373430b6d9a
Daniel Kocher <Daniel.Kocher@stud.sbg.ac.at>
2015-08-07 09:09:15 +0200

    Unroll Config type

--- types/types.go
+++ types/types.go
@@ -1,6 +1,8 @@
 package types
 
-type Config map[string][]map[string]string
+//type Config map[string][]map[string]string
+type ConfigRecord map[string]string
+type Config map[string][]ConfigRecord
 
 type ReadRecord struct {
 	Title string
