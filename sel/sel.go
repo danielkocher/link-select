8609fa5e133ddeace5152d7be6d7011cea067794
Daniel Kocher <Daniel.Kocher@stud.sbg.ac.at>
2015-08-07 17:13:04 +0200

    Implement first version of selectLink

--- sel/sel.go
+++ sel/sel.go
@@ -2,31 +2,68 @@ package sel
 
 import (
 	"os"
+	"os/exec"
 	"flag"
 	"fmt"
 	"log"
+	"time"
+	"math/rand"
 	"encoding/json"
 	"link-select/types"
 )
 
-func SelectLink(arg *flag.Flag) {
-	fmt.Fprintf(os.Stdout, "Selecting %s from json file\n", arg.Value)
+func SelectLink(arg *flag.Flag, file string, browser string) {
+	fmt.Fprintf(os.Stdout, "Selecting from %s\n", file)
 
-	readFile, err := os.Open("files/read.json")
+	// open JSON file
+	readFile, err := os.Open(file)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error while opening read JSON file\n")
+		fmt.Fprintf(os.Stderr, "Error while opening %s\n", file)
 		os.Exit(-1)
 	}
 
+	// decode JSON file into custom type ReadList
 	var readList types.ReadList
-	
 	jsonParser := json.NewDecoder(readFile)
 	if err = jsonParser.Decode(&readList); err != nil {
-		fmt.Fprintf(os.Stderr, "Error while parsing read JSON file\n")
+		fmt.Fprintf(os.Stderr, "Error while parsing %s\n", file)
 		log.Fatal(err)
+		readFile.Close()
 		os.Exit(-1)
 	}
 
-	//for i, v := range articles {
-	//fmt.Fprintf("title: %s, link: %s\n", article.Title, article.Link)
+	// set new seed to get non-deterministic random number generation
+	randSrc := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(randSrc)
+
+	// select random article
+	rndArticle := r.Intn(len(readList))
+	fmt.Fprintf(os.Stdout, "Selected \"%s\"\n", readList[rndArticle].Title)
+	
+	// open link in browser
+	cmd := exec.Command(browser, readList[rndArticle].Link)
+	err = cmd.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error while opening %s in %s\n",
+			readList[rndArticle], browser)
+		log.Fatal(err)
+		readFile.Close()
+		os.Exit(-1)
+	}
+
+	// remove selected article from list
+	readList = append(readList[:rndArticle], readList[rndArticle + 1:]...)
+
+	// write back JSON file without selected article
+	readFile.Close()
+	readFile, err = os.Create(file)
+	jsonWriter := json.NewEncoder(readFile)
+	if err = jsonWriter.Encode(&readList); err != nil {
+		fmt.Fprintf(os.Stderr, "Error while writing back %s\n", file)
+		log.Fatal(err)
+		readFile.Close()
+		os.Exit(-1)
+	}
+
+	readFile.Close()
 }
